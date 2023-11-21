53eb7a265828b37b5b0731866ae79928d4520226
Muneer <shaikmuneer@gmail.com>
2023-11-21 22:24:55 +0000

    add more tests in login spec

--- cypress/e2e/login.test.cy.js
+++ cypress/e2e/login.test.cy.js
@@ -5,8 +5,15 @@ describe('login', () => {
         cy.visit('https://magento.softwaretestingboard.com/customer/account/login')
     })
 
-    it('successful login', () => {      
+    it('successful login', () => {  
+        //using the cy.login custom command    
         cy.login('test123@test12.com', 'test@1234')
         cy.get(':nth-child(2) > .greet > .logged-in').should('have.text', 'Welcome, test test!')
+    })
+
+    it('unsuccessful login - error message', () => {
+        cy.login('test123@test1.com', 'test@1234')
+        cy.get('.message-error > div')
+        .should('contain.text', 'The account sign-in was incorrect or your account is disabled temporarily. Please wait and try again later.')
     })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -11,16 +11,3 @@ Cypress.Commands.add('login', (username, pwd) => {
     
     cy.get('.login.primary').click()
 })
-// Cypress.Commands.add('login', (email, password) => { ... })
-
-
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-
-
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-
-
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
